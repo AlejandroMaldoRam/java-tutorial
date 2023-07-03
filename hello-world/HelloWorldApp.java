52207be175da2de3a5564b9da1370224a2c316d5
Alejandro Maldonado <alejandro.maldonado@ciiia.mx>
2023-07-03 12:05:46 -0600

    Add helloworld app

---
+++ hello-world/HelloWorldApp.java
@@ -0,0 +1,9 @@
+/**
+ * This examples implements the classic Hello World application.
+ */
+
+ class HelloWorldApp{
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }
+ }
